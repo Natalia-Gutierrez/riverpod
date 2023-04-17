e82bd776edeb535589983a6c9d28aa7f737c39e4
Natalia <bgutisol2019@gmail.com>
2023-04-16 22:48:57 -0400

    final

--- flutter_application_1/lib/imagen.dart
+++ flutter_application_1/lib/imagen.dart
@@ -1,29 +1,73 @@
-import 'dart:convert';
+import 'dart:html';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ImageProvider extends ChangeNotifier {
-  String _imageUrl='';
-
-  String get imageUrl => _imageUrl;
-
-  void setImageUrl(String url) {
-    _imageUrl = url;
-    notifyListeners();
-  }
-  
-}
-final imageProvider = StateProvider((ref) => ImageProvider());
-
-void getImageUrl(ProviderReference ref) async {
+final imgRandom = FutureProvider.autoDispose<String>((ref) async {
   final response = await http.get(Uri.parse('https://dog.ceo/api/breeds/image/random'));
   if (response.statusCode == 200) {
-    final data = json.decode(response.body);
-    final imageUrl = data['message'];
-    ref.read(imageProvider).setImageUrl(imageUrl);
+    final json = jsonDecode(response.body);
+    return json['message'];
   } else {
-    print('Failed to load image');
+    throw Exception('Error al cargar imagen');
+  }
+});
+
+class Imagen extends StatefulWidget {
+  const Imagen({super.key});
+
+  @override
+  State<Imagen> createState() => _ImagenState();
+}
+
+class _ImagenState extends State<Imagen> {
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  width: 10,
+                ),
+                const Text(
+                  'Imagen Perrito Random',
+                  style: TextStyle(color: Colors.black) , 
+                  
+                ),
+              ]
+            ),
+            backgroundColor: Color.fromARGB(255, 5, 216, 146)         
+            ),
+          body: Center(
+            child: Consumer(
+              builder: (context, watch, _) {
+                final imageAsyncValue = watch(imgRandom);
+                return imageAsyncValue.when(
+                  data: (imageUrl) => Image.network(imageUrl),
+                  loading: () => CircularProgressIndicator(),
+                  error: (error, stackTrace) => Text('Error al cargar imagen'),
+                );
+              },
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              context.refresh(imgRandom);
+            },
+            tooltip: 'Refresh',
+            child: Icon(Icons.refresh),
+            backgroundColor: Color.fromARGB(255, 5, 216, 146)
+          ),
+        ),
+      ),
+    );
   }
 }
 
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,49 +1,22 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'imagen.dart';
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Generar imagen perrito',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Generar imagen perrito'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Consumer(
-              builder: (context, watch, _) {
-                final imageUrl = watch.watch(imageProvider).imageUrl;
-                
-                return imageUrl != null
-                    ? Image.network(
-                        imageUrl,
-                        height: 40,
-                        width: 40,
-                      )
-                    : Container();
-              },
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                getImageUrl(context as ProviderReference);
-              },
-              child: Text('Generate Dog Image'),
-            ),
-          ],
-        ),
-      ),
+    return const MaterialApp(
+      home: Imagen(),
     );
   }
 }
